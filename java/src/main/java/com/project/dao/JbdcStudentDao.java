b61a6e5e0258fe9f00f9d0f51a6e05aa35d34f61
TMNTony <121651052+TMNTony@users.noreply.github.com>
2023-09-11 10:57:38 -0500

    Add tables for students, majors, and their bridges

--- java/src/main/java/com/project/dao/JbdcStudentDao.java
+++ java/src/main/java/com/project/dao/JbdcStudentDao.java
@@ -1,13 +1,21 @@
 package com.project.dao;
 
 import com.project.model.Student;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 public class JbdcStudentDao implements StudentDao {
+    private final JdbcTemplate jdbcTemplate;
+
+    public JbdcStudentDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
     @Override
     public void createStudent(Student student) {
+        String sql = "INSERT INTO students (first_name, last_name, major_id, advisor_id) VALUES (?, ?, ?, ?)";
 
+        jdbcTemplate.update(sql, student.getFirstName(), student.getLastName(), student.getMajorId(), student.getAdvisorId())
     }
 }
--- java/src/main/java/com/project/model/Student.java
+++ java/src/main/java/com/project/model/Student.java
@@ -13,4 +13,43 @@ public class Student {
         this.majorId = majorId;
         this.advisorId = advisorId;
     }
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getMajorId() {
+        return majorId;
+    }
+
+    public void setMajorId(int majorId) {
+        this.majorId = majorId;
+    }
+
+    public int getAdvisorId() {
+        return advisorId;
+    }
+
+    public void setAdvisorId(int advisorId) {
+        this.advisorId = advisorId;
+    }
 }
